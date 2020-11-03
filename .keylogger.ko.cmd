c48722854044e63348d8658f153033c569b79a75
itamar <itamarmaouda101@gmail.com>
2020-11-03 09:33:33 +0200

    a

--- .keylogger.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/itamar/2/keylogger/keylogger.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/itamar/2/keylogger/keylogger.ko /home/itamar/2/keylogger/keylogger.o /home/itamar/2/keylogger/keylogger.mod.o;  true
--- .keylogger.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/itamar/2/keylogger/keylogger.mod := { echo  /home/itamar/2/keylogger/keylogger.o; echo; } > /home/itamar/2/keylogger/keylogger.mod
